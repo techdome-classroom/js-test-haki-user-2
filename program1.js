8e9e2c9948e048a987daa85eb9e7321c9d520110
haki-user <adityapratapsingh33@outlook.com>
2024-07-25 19:20:35 +0530

    modified:   program1.js 	modified:   program2.js

--- program1.js
+++ program1.js
@@ -1,8 +1,31 @@
-const getTotalIsles = function (grid) {
+const dfs = (i, j, grid, vis) => {
+  if(i<0 || j<0 || i===grid.length || j===grid[0].length || vis[i][j]===1 || grid[i][j]==="W") return 0;
+  vis[i][j] = 1;
+  grid[i][j]==="L"?1:0;
+  dfs(i+1, j, grid, vis);
+  dfs(i, j+1, grid, vis);
+  dfs(i-1, j, grid, vis);
+  dfs(i, j-1, grid, vis);
+}
 
-
-  // write your code here
-
+// const getTotalIsles = function (grid) {
+  const getTotalIsles = function (grid) {
+  let n = grid.length;
+  let m = grid[0].length;
+  const vis = Array.from({ length: n }, () => Array(m).fill(0));
+  let ans = 0;
+  for(let i = 0; i<n; i++) {
+    for(let j = 0; j<m; j++) {
+      if(vis[i][j]===1 || grid[i][j]==="W") continue;
+      ans++;
+      dfs(i, j, grid, vis);
+    }
+  }
+  // console.log({ ans });
+  return ans;
 };
 
-module.exports = getTotalIsles;
+// getTotalIsles([["L", "L"], ["W", "W"]]);
+
+// module.exports = getTotalIsles;
+  module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,41 @@
 const decodeTheRing = function (s, p) {
+  let n = s.length;
+  let m = p.length;
+  let i = 0;
+  console.log({s, p})
+  let j = 0;
+  while(j<m && i<n) {
+    while(i<n && j<m && p[j]===s[i]) {
+      i++;
+      j++;
+    }
+    if(p[j] === "?") {
+      i++;
+      j++;
+    } else if(p[j] === "*") {
+      // while(j<m && p[j]==="*") {
+      //   j++;
+      // }
+      let k = m-1;
+      while(k>=j && p[k]!=="*") {
+        k--;
+      }
+      // console.log({k, j, m})
+      j = k;
+      k = m-1;
+      let z = n-1;
+      while(j<k && i<z && s[z]===p[k]) {
+        // console.log({j, k, z, i, m});
+        k--;
+        z--;
+      }
+      // console.log({j, k, m})
+      return j===k;
+    } else j++;
+  }
+  return i===n && j===m;
+};
 
-    // write your code here
-
-  };
+console.log(decodeTheRing("abc", "a*"));
   
   module.exports = decodeTheRing;
